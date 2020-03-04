bbb947a43c0157a00068e5e9b5d6afa97fd7b9e7
charlie8888 <calic333@gmail.com>
2020-03-04 16:28:48 +0100

    Zadatak FizBuzz

--- index.js
+++ index.js
@@ -7,3 +7,17 @@
 
 
 //Zadatak broj 2. FizzBuzz  
+
+for(broj = 1; broj <101; broj++){
+    if(broj % 15==0){
+        console.log('FizzBuzz');
+    }
+    else if(broj % 3 == 0) {
+        console.log('Fizz');
+    }else if(broj % 5 == 0){
+        console.log('Buzz');
+  
+    } else {
+        console.log(broj);
+    }
+}
