887b900721c887ebb05cbe1f5132b807adc398e5
charlie8888 <calic333@gmail.com>
2020-03-05 23:06:19 +0100

    Zadatak 3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 
 //Zadatak broj 2. FizzBuzz  
-
+/*
 for(broj = 1; broj <101; broj++){
     if(broj % 15==0){
         console.log('FizzBuzz');
@@ -20,4 +20,51 @@ for(broj = 1; broj <101; broj++){
     } else {
         console.log(broj);
     }
+}; */
+
+// zadatak broj 3. Sahovska tabla
+
+
+var size = 8; //this is the variable setting
+
+var board = "";//this is the empty string we're going to add either ' ' , '#' or newline
+
+for (var y = 0; y < size; y++) {   /*in the outer loop we add newline to seperate rows*/
+  for (var x = 0; x < size; x++) {/*every inner loop rappresents a line, and alternatively it's adding either ' ' or '#' to the string that's being populated*/
+    if ((x + y) % 2 == 0)
+      board += " ";
+    else
+      board += "#";
+  }
+  board += "\n";
 }
+
+console.log(board);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
